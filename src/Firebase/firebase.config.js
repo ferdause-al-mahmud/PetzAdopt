2089ca648402b806890ae035cc41a5b6c80faf83
mahmud444t <ferdausemahmud@gmail.com>
2024-06-01 02:11:06 +0600

    project setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSVf0Z3tRMa6TdoUXYZfj1HZghxna82zM",
+    authDomain: "petzadopt-7acf4.firebaseapp.com",
+    projectId: "petzadopt-7acf4",
+    storageBucket: "petzadopt-7acf4.appspot.com",
+    messagingSenderId: "892259152433",
+    appId: "1:892259152433:web:1843c072661350b9d09b10"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
