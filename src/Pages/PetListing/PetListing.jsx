ac9c6b6a8baeea04952c36102ae28a54a234e8c6
mahmud444t <ferdausemahmud@gmail.com>
2024-06-18 20:23:19 +0600

    implemented infinite scrolling and added backend link

--- src/Pages/PetListing/PetListing.jsx
+++ src/Pages/PetListing/PetListing.jsx
@@ -1,21 +1,33 @@
 import { FaFilter } from "react-icons/fa";
 import PetListCard from "../../Components/Cards/PetListCard";
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import useAxiosCommon from "../../hooks/useAxiosCommon";
 import { useState } from "react";
 import 'react-loading-skeleton/dist/skeleton.css'
 import CardSkeleton from "../../Components/Skeleton/CardSkeleton";
+import InfiniteScroll from 'react-infinite-scroll-component';
 const PetListing = () => {
     const axiosCommon = useAxiosCommon();
     const [filter, setFilter] = useState('');
     const [search, setSearch] = useState('');
-    const { data: pets = [], isLoading } = useQuery({
+    const ITEMS_PER_PAGE = 6;
+
+    const fetchPets = async ({ pageParam = 0 }) => {
+        const { data } = await axiosCommon.get(`/pets?category=${filter}&name=${search}&limit=${ITEMS_PER_PAGE}&offset=${pageParam}`);
+        return { pets: data, nextPage: pageParam + ITEMS_PER_PAGE };
+    };
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isLoading,
+    } = useInfiniteQuery({
         queryKey: ['pets', filter, search],
-        queryFn: async () => {
-            const { data } = await axiosCommon.get(`/pets?category=${filter}&name=${search}`)
-            return data
-        },
-    })
+        queryFn: fetchPets,
+        getNextPageParam: (lastPage) => lastPage.pets.length === ITEMS_PER_PAGE ? lastPage.nextPage : undefined,
+    });
+
     const handleFilterChange = (event) => {
         const selectedFilter = event.target.value;
         // console.log('Selected filter:', selectedFilter);
@@ -57,18 +69,30 @@ const PetListing = () => {
                         </label>
                     </div>
                 </div>
-                <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {isLoading ? <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 
-                    {isLoading ? (
+                    {
                         Array(6).fill().map((_, index) => (
                             <CardSkeleton key={index} />
                         ))
-                    ) : (
-                        pets.map((pet, index) => (
-                            <PetListCard key={index} pet={pet} />
-                        ))
-                    )}
-                </div>
+                    }
+                </div> : <InfiniteScroll
+                    dataLength={data.pages.flat().length}
+                    next={fetchNextPage}
+                    hasMore={hasNextPage}
+                    loader={<div className=" mt-6 flex items-center justify-center"><span className="loading loading-spinner loading-lg"></span>
+                    </div>}
+
+                >
+                    <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {data.pages.flat().map((page) =>
+                            page.pets.map((pet, index) => (
+                                <PetListCard key={index} pet={pet} />
+                            ))
+                        )}
+                    </div>
+
+                </InfiniteScroll>}
 
 
             </div>
